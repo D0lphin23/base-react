6f8bf7b746073f50b49c747c74ab46bb08d0c7eb
gorelik.e <gorelikev23@gmail.com>
2026-03-20 22:30:05 +0500

    create app react

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const elem = (
+    <div>
+        <h1>Hello World</h1>
+    </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
