22c6d21fe954a1f329964418a1e249ca0ffa9ea3
gorelik.e <gorelikev23@gmail.com>
2026-03-21 15:52:54 +0500

    create class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import { Component } from "react";
 import "./App.css";
 
-const WhoAmI = ({ name, surname, link }) => {
-    return (
-        <div>
-            <h1>
-                My name is {name()}, surname - {surname.lastName}
-            </h1>
-            <a href={link}>My profile</a>
-        </div>
-    );
-};
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 31,
+            text: "+++",
+        };
+    }
+
+    nextYear = () => {
+        this.setState((state) => ({
+            years: state.years + 1,
+        }));
+    };
+
+    render() {
+        const { name, surname, link } = this.props;
+        return (
+            <div>
+                <h1>
+                    My name is {name()}, surname - {surname.lastName}, age -{" "}
+                    {this.state.years}
+                </h1>
+                <a href={link}>My profile</a>
+                <button onClick={this.nextYear}>{this.state.text}</button>
+            </div>
+        );
+    }
+}
 
 function App() {
     return (
