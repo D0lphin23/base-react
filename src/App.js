1f211910ba6abe59eca5666ae5928746649e7e62
gorelik.e <gorelikev23@gmail.com>
2026-04-29 21:32:26 +0500

    - Реализация HOC (Компонентов высшего порядка)

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import { useState, useReducer } from "react";
+import { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import "./App.css";
 
-const reducer = (state, action) => {
-    switch (action.type) {
-        case "toggle":
-            return { autoplay: !state.autoplay };
-        case "slow":
-            return { autoplay: 300 };
-        case "fast":
-            return { autoplay: 700 };
-        case "custom":
-            return { autoplay: action.payload };
-        default:
-            throw new Error("Unknown action type");
-    }
+const withSlider = (BaseComponent, getData) => {
+    return (props) => {
+        const [slide, setSlide] = useState(0);
+        const [autoplay, setAutoplay] = useState(false);
+
+        useEffect(() => {
+            setSlide(getData());
+        }, []);
+
+        function changeSlide(i) {
+            setSlide((slide) => slide + i);
+        }
+
+        return (
+            <BaseComponent
+                {...props}
+                slide={slide}
+                autoplay={autoplay}
+                setAutoplay={setAutoplay}
+                changeSlide={changeSlide}
+            />
+        );
+    };
 };
 
-const init = (initial) => {
-    return { autoplay: initial };
+const getDataFromFirstFetch = () => {
+    return 10;
+};
+const getDataFromSecondFetch = () => {
+    return 20;
 };
 
-const Slider = ({ initial }) => {
-    const [slide, setSlide] = useState(0);
-    // const [autoplay, setAutoplay] = useState(false);
-    const [state, dispatch] = useReducer(reducer, initial, init);
-
-    function changeSlide(i) {
-        setSlide((slide) => slide + i);
-    }
-
+const SliderFirst = (props) => {
     return (
         <Container>
             <div className="slider w-50 m-auto">
@@ -39,50 +44,20 @@ const Slider = ({ initial }) => {
                     alt="slide"
                 />
                 <div className="text-center mt-5">
-                    Active slide {slide} <br />
-                    {state.autoplay ? "auto" : null}{" "}
+                    Active slide {props.slide}
                 </div>
                 <div className="buttons mt-3">
                     <button
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(-1)}
+                        onClick={() => props.changeSlide(-1)}
                     >
                         -1
                     </button>
                     <button
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(1)}
+                        onClick={() => props.changeSlide(1)}
                     >
                         +1
-                    </button>
-                    <button
-                        className="btn btn-primary me-2"
-                        onClick={() => dispatch({ type: "toggle" })}
-                    >
-                        toggle autoplay
-                    </button>
-                    <button
-                        className="btn btn-primary me-2"
-                        onClick={() => dispatch({ type: "slow" })}
-                    >
-                        slow autoplay
-                    </button>
-                    <button
-                        className="btn btn-primary me-2"
-                        onClick={() => dispatch({ type: "fast" })}
-                    >
-                        fast autoplay
-                    </button>
-                    <button
-                        className="btn btn-primary me-2"
-                        onClick={(e) =>
-                            dispatch({
-                                type: "custom",
-                                payload: +e.target.textContent,
-                            })
-                        }
-                    >
-                        1000
                     </button>
                 </div>
             </div>
@@ -90,8 +65,71 @@ const Slider = ({ initial }) => {
     );
 };
 
+const SliderSecond = (props) => {
+    return (
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img
+                    className="d-block w-100"
+                    src="https://moya-planeta.ru/upload/images/xl/f2/35/f23517edb8332a14cb688c305f1e4bb6f97e4658.jpg"
+                    alt="slide"
+                />
+                <div className="text-center mt-5">
+                    Active slide {props.slide} <br />
+                    {props.autoplay ? "auto" : null}{" "}
+                </div>
+                <div className="buttons mt-3">
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(-1)}
+                    >
+                        -1
+                    </button>
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(1)}
+                    >
+                        +1
+                    </button>
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={() =>
+                            props.setAutoplay((autoplay) => !props.autoplay)
+                        }
+                    >
+                        toggle autoplay
+                    </button>
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+const SliderWithFirstFetch = withSlider(SliderFirst, getDataFromFirstFetch);
+const SliderWithSecondFetch = withSlider(SliderSecond, getDataFromSecondFetch);
+
+const withLogger = (WrappedComponent) => (props) => {
+    useEffect(() => {
+        console.log("render");
+    }, [props]);
+
+    return <WrappedComponent {...props} />;
+};
+
+const Hello = () => {
+    return <h1>Hello</h1>;
+};
+
+const HelloWithLogger = withLogger(Hello);
+
 function App() {
-    return <Slider initial={false} />;
+    return (
+        <>
+            <HelloWithLogger />
+            <SliderWithFirstFetch />
+            <SliderWithSecondFetch />
+        </>
+    );
 }
 
 export default App;
